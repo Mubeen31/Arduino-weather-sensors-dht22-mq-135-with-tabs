264d6ac91f80dc35f41fd43d4bf21cf540c326d7
Mubeen31 <qs6272527@gmail.com>
2022-12-26 22:50:51 +0000

    first commit

--- index.py
+++ index.py
@@ -219,6 +219,6 @@ def update_value(n_intervals):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True,
-                   # host='0.0.0.0', port=8080
+    app.run_server(debug=False,
+                   host='0.0.0.0', port=8080
                    )
